dc646fdacf3ca9e26d40e553e0b27b4bdc704bb8
Matthew Jones <matts3371@gmail.com>
2020-03-28 22:07:05 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,5 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
-  get '/students', to 
+  get 'students', to: 
 end
